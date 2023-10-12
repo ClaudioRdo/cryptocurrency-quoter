1210944ec6bdd408b6aef4ac380e161e0b9107c2
Robledo Claudio <claudio.n.robledo@gmail.com>
2023-10-11 23:49:07 -0300

    data currencies

---
+++ src/data/currencies.js
@@ -0,0 +1,10 @@
+const currencies = [
+    { id: 'USD', name: 'United States dollar'},
+    { id: 'MXN', name: 'Mexican peso'},
+    { id: 'EUR', name: 'Euro'},
+    { id: 'GBP', name: 'Pound Esterlin'},
+];
+
+export {
+    currencies
+};
